9a506ddea2dc996e1900979fb22209f40f49d6f1
Chris <chrisjones170187@gmail.com>
2026-01-09 21:54:33 +0000

    Game and Game Modes

--- games/PingPong/PingPongGame.cs
+++ games/PingPong/PingPongGame.cs
@@ -1,16 +1,42 @@
 using System.Diagnostics;
 using Axis.Engine;
+using Axis.Engine.Input;
+using Raylib_cs;
 namespace PingPong;
 
-internal sealed class PingPongGame : IGameInstance
+internal enum PingPongGameState
+{
+    Initialisation,
+    StartMenu,
+    Playing,
+}
+
+interface IPingPongGame
+{
+
+}
+
+internal sealed class PingPongGame : IPingPongGame, IGameInstance
 {
     private IGameEngine? _engine;
+    private IGameMode? _gameMode;
+    private PingPongGameState _state;
+
+    public PingPongGame()
+    {
+        _engine = null;
+        _gameMode = null;
+        _state = PingPongGameState.Initialisation;
+    }
 
     public void OnEngineStart(IGameEngine engine)
     {
         Debug.Assert(_engine == null);
 
         _engine = engine;
+
+        Initialise();
+
         Console.WriteLine("PingPong Game Started");
     }
 
@@ -20,5 +46,72 @@ internal sealed class PingPongGame : IGameInstance
 
         _engine = null;
         Console.WriteLine("PingPong Game Stopped");
+    }
+
+    private void Initialise()
+    {
+        Debug.Assert(_engine != null);
+
+        _gameMode = new StartMenuGameMode(this, _engine);
+
+        SwitchGameMode(PingPongGameState.StartMenu, _gameMode);
+    }
+
+    private void SwitchGameMode(PingPongGameState state, IGameMode gameMode)
+    {
+        Debug.Assert(_engine != null);
+
+        _gameMode?.Deactivate();
+        _gameMode = gameMode;
+        _gameMode.Activate();
+        _state = state;
+    }
+}
+
+internal interface IGameMode
+{
+    void Activate();
+    void Deactivate();
+}
+
+internal sealed class StartMenuGameMode : IGameMode, IInputReceiver
+{
+    private readonly IPingPongGame _game;
+    private readonly IGameEngine _engine;
+
+
+    public StartMenuGameMode(IPingPongGame game, IGameEngine engine)
+    {
+        _game = game;
+        _engine = engine;
+    }
+
+    public void Activate()
+    {
+        _engine.Devices.KeyboardAndMouse.BindContext([
+            new KeyboardInputContext(
+                this,
+                keyDown: [],
+                keyPressed: [
+                    new KeyboardInputMapping(KeyboardKey.Enter, StartMenuActions.StartGame)
+                ]
+            )
+        ]);
+
+        Console.WriteLine("Start Menu Activated");
+    }
+
+    public void Deactivate()
+    {
+        _engine.Devices.KeyboardAndMouse.ClearContext();
+        Console.WriteLine("Start Menu Deactivated");
+    }
+
+    void IInputReceiver.ReceiveInput(InputEvent inputEvent)
+    {
+        if (inputEvent.Id == StartMenuActions.StartGame)
+        {
+            Console.WriteLine("Start Game action received!");
+        }
     }
 }
