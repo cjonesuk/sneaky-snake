55a71ce7c6f5eb741f98f1f5927156cf06677e8b
Chris <chrisjones170187@gmail.com>
2026-01-10 20:29:58 +0000

    Cleaning up

--- games/PingPong/PingPongGame.cs
+++ games/PingPong/PingPongGame.cs
@@ -1,12 +1,5 @@
 using System.Diagnostics;
-using System.Numerics;
-using Axis.ECS;
 using Axis.Engine;
-using Axis.Engine.Components;
-using Axis.Engine.Input;
-using Axis.Engine.Rendering;
-using Engine.Components;
-using Raylib_cs;
 namespace PingPong;
 
 internal enum PingPongGameState
@@ -16,10 +9,18 @@ internal enum PingPongGameState
     Playing,
 }
 
+internal interface IGameMode
+{
+    void Activate();
+    void Deactivate();
+}
+
+
 interface IPingPongGame
 {
 
 }
+
 
 internal sealed class PingPongGame : IPingPongGame, IGameInstance
 {
@@ -70,84 +71,5 @@ internal sealed class PingPongGame : IPingPongGame, IGameInstance
         _gameMode = gameMode;
         _gameMode.Activate();
         _state = state;
-    }
-}
-
-internal interface IGameMode
-{
-    void Activate();
-    void Deactivate();
-}
-
-internal static class WorldExtensions
-{
-    public static Entity SpawnCamera2d(this IWorld world, Vector2 position, float zoom)
-    {
-        return world.CreateEntity(new Transform2d(position), new Camera2d(zoom));
-    }
-
-    public static Entity SpawnBall(this IWorld world, Vector2 position, Color color)
-    {
-        return world.CreateEntity(
-            new Transform2d(position),
-            new BasicShape(ShapeType.Circle, new Vector2(10f, 10f),
-            color));
-    }
-}
-
-internal sealed class StartMenuGameMode : IGameMode, IInputReceiver
-{
-    private readonly IPingPongGame _game;
-    private readonly IGameEngine _engine;
-    private readonly IWorld _world;
-    private readonly Entity _camera;
-    private readonly Entity _ball;
-
-
-    public StartMenuGameMode(IPingPongGame game, IGameEngine engine)
-    {
-        _game = game;
-        _engine = engine;
-        _world = World.Create();
-        _camera = _world.SpawnCamera2d(new Vector2(400, 300), 1.0f);
-        _ball = _world.SpawnBall(new Vector2(400, 300), Color.Red);
-    }
-
-    public void Activate()
-    {
-        _engine.SetWorld(_world);
-
-        _engine.Devices.KeyboardAndMouse.BindContext([
-            new KeyboardInputContext(
-                this,
-                keyDown: [],
-                keyPressed: [
-                    new KeyboardInputMapping(KeyboardKey.Enter, StartMenuActions.StartGame)
-                ]
-            )
-        ]);
-
-        _engine.SetViewports(
-        [
-            Viewport.Fullscreen(_camera)
-        ]);
-
-        Console.WriteLine("Start Menu Activated");
-    }
-
-    public void Deactivate()
-    {
-        _engine.Devices.KeyboardAndMouse.ClearContext();
-        _engine.ClearWorld();
-
-        Console.WriteLine("Start Menu Deactivated");
-    }
-
-    void IInputReceiver.ReceiveInput(InputEvent inputEvent)
-    {
-        if (inputEvent.Id == StartMenuActions.StartGame)
-        {
-            Console.WriteLine("Start Game action received!");
-        }
     }
 }
---
+++ games/PingPong/WorldExtensions.cs
@@ -0,0 +1,29 @@
+using System.Numerics;
+using Axis.ECS;
+using Axis.Engine.Components;
+using Engine.Components;
+using Raylib_cs;
+namespace PingPong;
+
+internal static class WorldExtensions
+{
+    public static Entity SpawnCamera2d(this IWorld world, Vector2 position, float zoom)
+    {
+        return world.CreateEntity(new Transform2d(position), new Camera2d(zoom));
+    }
+
+    public static Entity SpawnBall(this IWorld world, Vector2 position, Color color)
+    {
+        return world.CreateEntity(
+            new Transform2d(position),
+            new BasicShape(ShapeType.Circle, new Vector2(10f, 10f),
+            color));
+    }
+
+    public static Entity SpawnPaddle(this IWorld world, Vector2 position, Color color)
+    {
+        return world.CreateEntity(
+            new Transform2d(position),
+            new BasicShape(ShapeType.Rectangle, new Vector2(10f, 60f), color));
+    }
+}
