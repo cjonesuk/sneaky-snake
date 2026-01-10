cdf49316f62b2c1b2929a3cc1bcea052f75c483a
Chris <chrisjones170187@gmail.com>
2026-01-10 15:26:16 +0000

    CommandPayload into its own file

--- src/Axis.Core/Collections/CommandBuffer.cs
+++ src/Axis.Core/Collections/CommandBuffer.cs
@@ -1,27 +1,6 @@
-using System.Runtime.CompilerServices;
 using Axis.Core.Memory;
 
 namespace Axis.Core.Collections;
-
-public unsafe readonly struct CommandPayload(void* payloadPtr)
-{
-    private readonly void* _payloadPtr = payloadPtr;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static CommandPayload From(void* basePtr, int offset)
-    {
-        void* payloadPtr = (byte*)basePtr + offset;
-        return new CommandPayload(payloadPtr);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public ref TPayload GetRef<TPayload>()
-        where TPayload : unmanaged
-    {
-        return ref Unsafe.AsRef<TPayload>(_payloadPtr);
-    }
-}
-
 
 public abstract class CommandQueue<TContext, TCommand>
 {
